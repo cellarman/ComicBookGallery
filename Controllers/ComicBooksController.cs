2422fa62be9dcd86dab35890d9e60235be391fae
IIIHorseman <keithrk1@gmail.com>
2020-03-09 20:04:16 -0400

    Added comic books controller

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;   //MVC Library inserted
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller   //Inherits from Controller base
+    {
+    }
+}
